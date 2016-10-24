23739abc2f6553f539220c7099a89b99839f8387
Darin Ma <darinmma@gmail.com>
2016-10-24 12:22:24 -0700

    add seeds and pry

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puppies = Puppy.create([
+  {name: 'Kona', breed: 'Pit Bull', age: 5, tricks: false},
+  {name: 'Powder', breed: 'Chihuahua Mix', age: 6, tricks: true},
+  {name: 'Bo', breed: 'Portuguese Water Dog', age: 8, tricks: true}
+  ])
